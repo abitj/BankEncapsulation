60d845e9a4111dace1565295dcffea842be3c122
volunteerocks <66223142+aabit@users.noreply.github.com>
2021-02-04 23:29:19 -0600

    Added BankEncapsulation classes

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var akoiAccount = new BankAccount();
+            Console.WriteLine("How much would like to deposit?");
+            var amountToDeposit = double.Parse(Console.ReadLine());
+            akoiAccount.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Thank you! Your balance is now {akoiAccount.GetBalance()}");
+
+
+
+
+        }
+    }
+}
